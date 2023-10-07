722ff040b907ba31fb7e875db067ccee85158fd5
adiyat-shihab <mohammadshihab104@gmail.com>
2023-10-07 18:24:24 +0600

    refactor service details and complete the service details

--- src/Component/ServiceDetails/ServiceDetails.jsx
+++ src/Component/ServiceDetails/ServiceDetails.jsx
@@ -9,14 +9,38 @@ const ServiceDetails = () => {
   const idString = parseInt(id);
   const specificData = loader.find((item) => item.id === idString);
   console.log(specificData);
-  const { name, price, short_description, image } = specificData;
+  const {
+    name,
+    price,
+    short_description,
+    details,
+    image,
+    match_duration,
+    platform,
+  } = specificData;
   return (
-    <div>
-      <div>
-        <div></div>
-        <div>
-          <img src={image} className="h-[33rem] w-[58rem]" alt="" />
+    <div className="bg-[#120e20] h-screen text-white">
+      <div className="px-[10rem] pt-20 flex justify-evenly ">
+        <div className="space-y-8 w-[41rem]">
+          <p className="font-bold text-3xl font-[Oswald]">Name: {name} </p>
+          <hr />
+          <p className="font-bold text-3xl font-[Oswald]">Fee: ${price}</p>
+          <hr />
+          <p className="font-bold text-3xl font-[Oswald]">
+            Match Duration: {match_duration}
+          </p>
+          <hr />
+          <p className="font-bold text-3xl font-[Oswald]">
+            Platform: {platform}
+          </p>
         </div>
+        <div>
+          <img src={image} className="h-[22rem] w-[41rem]" alt="" />
+        </div>
+      </div>
+      <div className="px-[10rem] pt-20 ">
+        <p className="font-bold text-6xl mb-5">About This Game</p>
+        <p className=" text-3xl">{details}</p>
       </div>
     </div>
   );
