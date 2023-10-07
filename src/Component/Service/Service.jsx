f9b5086ac399ac93b9d4e0bdd4451008da2afb7a
adiyat-shihab <mohammadshihab104@gmail.com>
2023-10-07 10:16:17 +0600

    customize the sevice card changing name short description price button

--- src/Component/Service/Service.jsx
+++ src/Component/Service/Service.jsx
@@ -5,11 +5,13 @@ const Service = ({ loader }) => {
   console.log(loader);
   return (
     <div className="bg-[#100A2E] p-10 px-60 ">
-      <h1 className="text-white  text-center text-6xl">Plan Your Epic Match</h1>
-      <p className="text-center text-3xl mt-10 text-white">
-        Join One of those matches and earned your reward
+      <h1 className="text-white  text-center text-6xl font-[Oswald]">
+        Plan Your Epic Match
+      </h1>
+      <p className="text-center text-3xl mt-10 text-white font-[Oswald] mb-10">
+        Join One of those matches and earned your Amazing Reward
       </p>
-      <div className=" grid grid-cols-2">
+      <div className=" grid grid-cols-2 gap-y-10 justify-items-center">
         {loader.map((data) => (
           <ServiceCard data={data}></ServiceCard>
         ))}
